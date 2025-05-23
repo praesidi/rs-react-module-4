a0cc5a3ccfed7fc03f6c4832af62138975d60952
praesidi <edik.halilov1999@gmail.com>
2025-05-23 19:53:03 +0300

    feat: add custom hook useWindowScroll

---
+++ src/hooks/useWindowScroll.ts
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { useWindowEvent } from "./useWindowEvent";
+
+type Scroll = {
+  x?: number;
+  y?: number;
+};
+
+type ReturnValue = [scroll: Scroll, scrollTo: (args: Scroll) => void];
+
+export const useWindowScroll = (): ReturnValue => {
+  const [scroll, setScroll] = useState<Scroll>({
+    x: 0,
+    y: 0,
+  });
+
+  const handleScrollTo = (newValue: Scroll) => {
+    window.scrollTo(newValue.x ?? 0, newValue.y ?? 0);
+  };
+
+  const handleUpdateScroll = () => {
+    setScroll({
+      x: window.scrollX,
+      y: window.scrollY,
+    });
+  };
+
+  useWindowEvent("scroll", handleUpdateScroll);
+
+  return [scroll, handleScrollTo];
+};
