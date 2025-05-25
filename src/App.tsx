2907ec19af503a0f16899132bb32155b9d3fdf2a
praesidi <edik.halilov1999@gmail.com>
2025-05-25 15:12:35 +0300

    feat: update page to test hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import "./App.css";
+import { useViewportSize } from "./hooks/useViewportSize";
 
 function App() {
-  return (
-    <>
-      <h1>Custom hooks overview</h1>
-      <div className="card">
-        <button onClick={() => {}}>Action</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+	const { height, width } = useViewportSize();
+
+	return (
+		<>
+			Width: {width}, height: {height}
+		</>
+	);
 }
 
 export default App;
